ebd8368566cb85069499b214b8cc637283ade8de
Qin CHEN@p15 <qchen2015@hotmail.com>
2025-04-25 10:55:06 +0800

    units documentation

--- src/units.rs
+++ src/units.rs
@@ -1,8 +1,10 @@
+#![doc = include_str!("../doc/qudt.md")]
+
 //! QUDT is "Quantities, Units, Dimensions and Data Types in OWL and XML"
 //!
 //! This module contains the QUDT unit definitions. using RDF/XML syntax to load the QUDT ontology
 //! Provides a set of unit definitions and conversion functions for various physical quantities
-//! 
+//!
 //! The QUDT ontology is a standard for representing units of measurement and physical quantities
 //! in RDF/XML format. It provides a comprehensive set of unit definitions and conversion functions
 //! for various physical quantities, including length, mass, time, temperature, and more.
@@ -15,16 +17,3 @@
 //! The QUDT ontology is available at: http://www.qudt.org/
 //!
 //! The QUDT ontology is licensed under the Apache License, Version 2.0. See the LICENSE file for details.
-
-
-//! allocate ontology path to the QUDT ontology file
-// fn find_ontology_path() -> Option<String> {
-//     let mut path = std::env::current_dir().ok()?;
-//     path.push("src/units/qudt.owl");
-//     if path.exists() {
-//         Some(path.to_str()?.to_string())
-//     } else {
-//         None
-//     }
-// }
-
